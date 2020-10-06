a1a46a0492d31d1330b7b918ecb60b52c16c8ac2
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-06 19:30:38 -0400

    Add componentDidMount

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -8,23 +8,22 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            movies: [],
+            movies: null
         };
     }
-    getAllMovies() {
+    componentDidMount() {
         return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
             .then((response) => response.json())
             .then((data) => {
-                this.state.movies = data.movies;
+                this.setState({movies: data.movies});
             });
     }
 
     render() {
-        this.getAllMovies()
         return (
             <div>
                 <MainView />
-                <CardsContainer />
+                <CardsContainer allMovies={this.state.movies} />
             </div>
         );
     }
