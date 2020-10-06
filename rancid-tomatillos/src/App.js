1e48416c0a8ed8286199aaf1a4d69420bf0b17d3
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-06 18:51:41 -0400

    Set initial movie state

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 import MainView from "./components/MainView";
+import CardsContainer from "./components/CardsContainer";
+
 import "./App.css";
 
-function App() {
-    return (
-        <div>
-            <MainView />
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [],
+        };
+    }
+    getAllMovies() {
+        return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                this.state.movies = data.movies;
+            });
+    }
+
+    render() {
+        this.getAllMovies()
+        return (
+            <div>
+                <MainView />
+                <CardsContainer />
+            </div>
+        );
+    }
 }
 
 export default App;
