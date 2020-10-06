54699d3f8eb6dee7ca21d107c867aa19958541cb
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-06 19:46:01 -0400

    Add functionality to cardsContainer

--- rancid-tomatillos/src/components/CardsContainer.js
+++ rancid-tomatillos/src/components/CardsContainer.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function CardsContainer(props) {
-    console.log(props)
-    return (
-        <h1>test</h1>
-    )
+    console.log(props);
+    let pCards
+    if (props.allMovies) {
+        pCards = props.allMovies.map((movie) => <p>{movie.title}</p>);
+    }
+    return <h1>{pCards}</h1>;
 }
 
-export default CardsContainer
-
+export default CardsContainer;
