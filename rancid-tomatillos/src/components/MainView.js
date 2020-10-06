dbf618c8d0c2ef96d52ac55abf7064f5c01003fd
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-06 17:57:06 -0400

    Connect header to app

---
+++ rancid-tomatillos/src/components/MainView.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header"
+//import CardContainer from "./CardContainer";
+
+function MainView() {
+    return (
+        <main>
+            <Header />
+        </main>
+    );
+}
+
+export default MainView;
