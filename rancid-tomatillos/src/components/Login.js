fd0b3bc3f4554c0a37fa4faa5c8ff84265940a4c
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-07 19:59:23 -0400

    Add initial login structure

--- rancid-tomatillos/src/components/Login.js
+++ rancid-tomatillos/src/components/Login.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React from "react";
 
 class Login extends React.Component {
     constructor() {
         super();
+        this.state = {
+            username: "",
+            password: "",
+        };
     }
+    updateValue = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    };
     render() {
+        console.log(this);
         return (
             <form>
                 <h1>Enter User Details </h1>
-                <input name="username" type="text" value={this.state.password}>Enter User Details </input>
-                <input name="password" type="text" value={this.state.password}>Enter User Details </input>
-                <h1>Enter User Details </h1>
+                <input
+                    name="username"
+                    type="text"
+                    onChange={this.updateValue}
+                    value={this.state.username}
+                ></input>
+                <input
+                    name="password"
+                    type="text"
+                    onChange={this.updateValue}
+                    value={this.state.password}
+                ></input>
+                <button>Enter User Details </button>
             </form>
-        )
+        );
     }
 }
 
 export default Login;
-
