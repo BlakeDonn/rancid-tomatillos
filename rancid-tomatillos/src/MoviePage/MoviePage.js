88de50f49400adeb74b18bf8acf4436de89a4433
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-11 17:05:34 -0400

    Finalize data flow of movie page

--- rancid-tomatillos/src/MoviePage/MoviePage.js
+++ rancid-tomatillos/src/MoviePage/MoviePage.js
@@ -1,14 +1,29 @@
 import React from "react";
 let movie;
 function MoviePage(props) {
+  console.log(props)
   let id = props.location.pathname.split("/")[2];
   const constMovieFetch = async () => {
     let promise = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     movie = await promise.json()
-    props.history.push(`/movie`)
+    props.history.push({
+      pathname: "/movie",
+      state: {
+        movie: movie.movie
+      },
+    });
+
   }
   if (props.match.isExact) {
-    return <h1>{movie.movie.title}</h1>
+    const {id, title, poster_path, backdrop_path, release_date} = props.location.state.movie
+    return (
+      <div
+        itemID={id}>
+        <h1>{title}</h1>
+        <img src={backdrop_path} alt={`${title} background`} />
+        <h1>{title}</h1>
+      </div>
+    )
   }
   else {
     constMovieFetch()
