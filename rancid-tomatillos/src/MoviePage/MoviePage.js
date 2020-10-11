b5e21801f7b487b4bb59a97ce29057789de2c75f
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-11 17:37:59 -0400

    Adjust movie page

--- rancid-tomatillos/src/MoviePage/MoviePage.js
+++ rancid-tomatillos/src/MoviePage/MoviePage.js
@@ -1,36 +1,51 @@
 import React from "react";
-let movie;
 function MoviePage(props) {
-  console.log(props)
   let id = props.location.pathname.split("/")[2];
   const constMovieFetch = async () => {
-    let promise = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    movie = await promise.json()
+    let promise = await fetch(
+      `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
+    );
+    let movie = await promise.json();
     props.history.push({
       pathname: "/movie",
       state: {
-        movie: movie.movie
+        movie: movie.movie,
       },
     });
-
-  }
+  };
   if (props.match.isExact) {
-    const {id, title, poster_path, backdrop_path, release_date} = props.location.state.movie
+    console.log(props);
+    const {
+      average_rating,
+      backdrop_path,
+      budget,
+      genres,
+      id,
+      overview,
+      poster_path,
+      release_date,
+      revenue,
+      runtime,
+      tagline,
+      title,
+    } = props.location.state.movie;
     return (
-      <div
-        itemID={id}>
-        <h1>{title}</h1>
+      <div itemID={id} onMouseOver={console.log(overview)}>
+        <h1 className="movie-title">{title}</h1>
         <img src={backdrop_path} alt={`${title} background`} />
-        <h1>{title}</h1>
+        <p className="average-rating"> Average user rating: {average_rating}</p>
+        <p className="genres"> {genres}</p>
+        <p className="release-date"> {release_date}</p>
+        <p className="revenue">{revenue ? revenue : "Not released"}</p>
+        <p className="runtime"> {runtime} minutes</p>
+        {budget ? <p>className="budget"{budget}</p> : null}
+        {tagline ? <p className="tagline"> {tagline}</p> : null}
       </div>
-    )
-  }
-  else {
-    constMovieFetch()
+    );
+  } else {
+    constMovieFetch();
     return <h1>Loading</h1>;
   }
-
-
 }
 
 export default MoviePage;
