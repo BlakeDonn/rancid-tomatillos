57d6c334acf22f5164c12d029b3e8ee8fbcf7f69
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-11 01:02:28 -0400

    Add contional rendering to moviepage

--- rancid-tomatillos/src/MoviePage/MoviePage.js
+++ rancid-tomatillos/src/MoviePage/MoviePage.js
@@ -1,12 +1,21 @@
 import React from "react";
-
+let movie;
 function MoviePage(props) {
-  console.log(props)
   let id = props.location.pathname.split("/")[2];
-  fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then((response) => response.json())
-    .then((movie) => console.log(movie));
-  return <h1>hi</h1>;
+  const constMovieFetch = async () => {
+    let promise = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    movie = await promise.json()
+    props.history.push(`/movie`)
+  }
+  if (props.match.isExact) {
+    return <h1>{movie.movie.title}</h1>
+  }
+  else {
+    constMovieFetch()
+    return <h1>Loading</h1>;
+  }
+
+
 }
 
 export default MoviePage;
