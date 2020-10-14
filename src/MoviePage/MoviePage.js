8a0f6a895748389757d4f5be5c31237ce6c2579c
Hanna Kim <hannakim91@gmail.com>
2020-10-14 17:10:29 -0600

    Add input and button for rating in MoviePage

--- src/MoviePage/MoviePage.js
+++ src/MoviePage/MoviePage.js
@@ -16,6 +16,9 @@ class MoviePage extends Component {
     const movie = await response.movie;
     this.setState({movie});
   }
+  rateMovie() {
+
+  }
   render() {
     return (
       <div itemID={this.state.movie.id}>
@@ -24,16 +27,19 @@ class MoviePage extends Component {
           alt={`backdrop of ${this.state.movie.title}`}
         ></img>
         <h3 className="movie-title">{this.state.movie.title}</h3>
-        <p className="avg-rating">{this.state.movie.average_rating}</p>
-        <p className="avg-rating">{this.state.movie.budget}</p>
-        <p className="avg-rating">{this.state.movie.genres}</p>
-        <p className="avg-rating">{this.state.movie.overview}</p>
-        <p className="avg-rating">{this.state.movie.release_date}</p>
-        <p className="avg-rating">{this.state.movie.revenue}</p>
-        <p className="avg-rating">{this.state.movie.runtime} minutes</p>
-        <p className="avg-rating">{this.state.movie.tagline}</p>
-        <p className="avg-rating">{this.state.movie.tagline}</p>
+        <p>{this.state.movie.average_rating}</p>
+        <p>{this.state.movie.budget}</p>
+        <p>{this.state.movie.genres}</p>
+        <p>{this.state.movie.overview}</p>
+        <p>{this.state.movie.release_date}</p>
+        <p>{this.state.movie.revenue}</p>
+        <p>{this.state.movie.runtime} minutes</p>
+        <p>{this.state.movie.tagline}</p>
+        <p>{this.state.movie.tagline}</p>
         <p className="user-rating">"Your rating"</p>
+        <label>Rating this movie - 1(hate) - 10 (love)</label>
+        <input type="number" min="1" max="10" required="required"></input>
+        <button onClick={this.rateMovie}>Rate Movie</button>
       </div>
     );
   }
