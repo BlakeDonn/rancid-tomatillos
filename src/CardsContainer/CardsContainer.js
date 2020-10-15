1d79def64ce37a0058fa40c22cd6e8b0275253a2
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-15 01:29:18 -0400

    Add conditional for preview rating render

--- src/CardsContainer/CardsContainer.js
+++ src/CardsContainer/CardsContainer.js
@@ -2,17 +2,15 @@ import React from "react";
 import PreviewCard from "../PreviewCard/PreviewCard";
 
 function CardsContainer(props) {
+  let ratedMovie;
   let pCards = props.movies.map((movie) => {
-    let ratedMovie = props.userRatings.find(x => x.movie_id === movie.id)
-    if (ratedMovie) {
-      movie.userRating = ratedMovie.rating
+    if (props.userRatings) {
+      ratedMovie = props.userRatings.find((x) => x.movie_id === movie.id);
     }
-    return (
-      <PreviewCard
-        history={props.history}
-        {...movie}
-      />
-    );
+    if (ratedMovie) {
+      movie.userRating = ratedMovie.rating;
+    }
+    return <PreviewCard history={props.history} {...movie} />;
   });
 
   return <div className="container">{pCards}</div>;
