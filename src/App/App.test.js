1d00c62b8c08b93dd623c627960b0bb1813c23c6
Hanna Kim <hannakim91@gmail.com>
2020-10-16 19:31:31 -0600

    Edit App.test.js

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,40 +1,74 @@
 import React from "react";
 import App from "./App";
-import {render, screen, waitFor} from "@testing-library/react";
+import {render, screen, waitFor, cleanup} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import {Router} from "react-router-dom";
+import {Router, MemoryRouter } from "react-router-dom";
 import {createMemoryHistory} from "history";
 import {getAllMovies} from "../api";
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 jest.mock("../api.js");
 
-describe("App", () => {
+describe("Header", () => {
+
+  getAllMovies.mockResolvedValueOnce({
+    movies: [
+      {
+        average_rating: 6.8,
+        backdrop_path:
+          "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        id: 694919,
+        poster_path:
+          "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        release_date: "2020-09-29",
+        title: "Money Plane",
+      },
+    ],
+  });
+  
+  it("Should have a header with links", () => {
+    render (
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+    )
+
+    expect(screen.getByText('Rotten Tomatillos')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    screen.debug()
+    // expect(screen.getByRole('link', {name: /login/})).toBeInTheDocument();
+  })
+})
+
+describe("Dashboard", () => {
+
+  getAllMovies.mockResolvedValueOnce({
+    movies: [
+      {
+        average_rating: 6.8,
+        backdrop_path:
+          "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        id: 694919,
+        poster_path:
+          "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        release_date: "2020-09-29",
+        title: "Money Plane",
+      },
+    ],
+  });
+
   it("Should load movies on mount", async () => {
-    getAllMovies.mockResolvedValueOnce({
-      movies: [
-        {
-          average_rating: 6.8,
-          backdrop_path:
-            "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          id: 694919,
-          poster_path:
-            "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          release_date: "2020-09-29",
-          title: "Money Plane",
-        },
-      ],
-    });
     render(<App />);
     const idea = await waitFor(() => screen.getByText("Money Plane"));
     expect(idea).toBeInTheDocument()
   });
+
   it("User is redirected to login page on link click", async () => {
     const history = createMemoryHistory();
     render(
       <Router history={history}>
         <App />
-      </Router>
+      </Router>    
     );
     userEvent.click(screen.getByRole("link", {name: "Login"}));
     expect(screen.getByText(/login/i)).toBeInTheDocument();
@@ -46,3 +80,5 @@ describe("App", () => {
   userEvent.type(screen.getByPlaceholderText("password"), "test");
   userEvent.click(screen.getByRole("button", {name: "Submit"}));*/
 });
+
+
