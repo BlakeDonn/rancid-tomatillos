b6e1a255fbf8533701c711704fcda68bf0b1a5a2
Hanna Kim <hannakim91@gmail.com>
2020-10-16 19:55:14 -0600

    Nest header and dashboard tests in app block

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -10,61 +10,66 @@ import "@testing-library/jest-dom";
 jest.mock("../api.js");
 
 // needs to get called each time you call "getAllMovies" within App 
-getAllMovies.mockResolvedValue({
-  movies: [
-    {
-      average_rating: 6.8,
-      backdrop_path:
-        "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-      id: 694919,
-      poster_path:
-        "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-      release_date: "2020-09-29",
-      title: "Money Plane",
-    },
-  ],
-});
-
-describe("Header", () => {
-//afterEach(cleanup) cleans DOM - not stubs created using data "retrieved" from jest.mock
-
-  it("Should have a header with links", () => {
-    render (
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-    )
-
-    expect(screen.getByText('Rotten Tomatillos')).toBeInTheDocument();
-    expect(screen.getByText('Home')).toBeInTheDocument();
-    expect(screen.getByRole('link', {name: /Login/})).toBeInTheDocument();
+describe("App", () => {
+  beforeEach(() => {
+    getAllMovies.mockResolvedValue({
+      movies: [
+        {
+          average_rating: 6.8,
+          backdrop_path:
+            "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+          id: 694919,
+          poster_path:
+            "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+          release_date: "2020-09-29",
+          title: "Money Plane",
+        },
+      ],
+    });
   })
+  
+  describe("Header", () => {
+  //afterEach(cleanup) cleans DOM - not stubs created using data "retrieved" from jest.mock
+  
+    it("Should have a header with links", () => {
+      render (
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+      )
+  
+      expect(screen.getByText('Rotten Tomatillos')).toBeInTheDocument();
+      expect(screen.getByText('Home')).toBeInTheDocument();
+      expect(screen.getByRole('link', {name: /Login/})).toBeInTheDocument();
+    })
+  })
+  
+  describe("Dashboard", () => {
+  
+    it("Should load movies on mount", async () => {
+      render(<App />);
+      const idea = await waitFor(() => screen.getByText("Money Plane"));
+      expect(idea).toBeInTheDocument()
+    });
+  
+    it("User is redirected to login page on link click", async () => {
+      const history = createMemoryHistory();
+      render(
+        <Router history={history}>
+          <App />
+        </Router>    
+      );
+      userEvent.click(screen.getByRole("link", {name: "Login"}));
+      expect(screen.getByText(/login/i)).toBeInTheDocument();
+      expect(screen.getByRole("button", {name: "Submit"})).toBeInTheDocument();
+    });
+  
+    /*userEvent.click(screen.getByRole("link", {name: "Login"}));
+    userEvent.type(screen.getByPlaceholderText("email"), "marge@turing.io");
+    userEvent.type(screen.getByPlaceholderText("password"), "test");
+    userEvent.click(screen.getByRole("button", {name: "Submit"}));*/
+  });
 })
 
-describe("Dashboard", () => {
-
-  it("Should load movies on mount", async () => {
-    render(<App />);
-    const idea = await waitFor(() => screen.getByText("Money Plane"));
-    expect(idea).toBeInTheDocument()
-  });
-
-  it("User is redirected to login page on link click", async () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={history}>
-        <App />
-      </Router>    
-    );
-    userEvent.click(screen.getByRole("link", {name: "Login"}));
-    expect(screen.getByText(/login/i)).toBeInTheDocument();
-    expect(screen.getByRole("button", {name: "Submit"})).toBeInTheDocument();
-  });
-
-  /*userEvent.click(screen.getByRole("link", {name: "Login"}));
-  userEvent.type(screen.getByPlaceholderText("email"), "marge@turing.io");
-  userEvent.type(screen.getByPlaceholderText("password"), "test");
-  userEvent.click(screen.getByRole("button", {name: "Submit"}));*/
-});
 
 
