bb512f34730bfe00a8e933bd116a20e77e44ee77
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-20 16:25:15 -0400

    Add previewCard css

--- src/PreviewCard/PreviewCard.js
+++ src/PreviewCard/PreviewCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./PreviewCard.css"
 import {Link} from "react-router-dom";
 import {ReactComponent as Tomato} from "../Assets/tomato.svg";
 import {ReactComponent as UnTomato} from "../Assets/untomato.svg";
