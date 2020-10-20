424b8c9aacfde336406b4e86aed18e9eea6b6665
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-20 19:31:41 -0400

    Remove unused imports

--- src/PreviewCard/PreviewCard.test.js
+++ src/PreviewCard/PreviewCard.test.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
-import {createMemoryHistory} from "history";
-import userEvent from "@testing-library/user-event";
 import PreviewCard from "./PreviewCard";
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
