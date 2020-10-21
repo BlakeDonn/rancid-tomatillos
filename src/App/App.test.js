0710ca938b80227cc14947e323404bfc7c686fd5
Hanna Kim <hannakim91@gmail.com>
2020-10-20 18:45:33 -0600

    Build out integration test for movie show page after login

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -3,7 +3,7 @@ import App from "./App";
 import {render, screen, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
-import { getAllMovies, getIndividualMovie, postUserLogin, getUserRatings } from "../api";
+import { getAllMovies, getIndividualMovie, postUserLogin, getUserRatings, getMovieComments } from "../api";
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 jest.mock("../api.js");
@@ -22,7 +22,15 @@ describe("App", () => {
           release_date: "2020-09-29",
           title: "Money Plane",
         },
-      ],
+        {
+          "id": 337401,
+          "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+          "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+          "title": "Mulan",
+          "average_rating": 5.666666666666667,
+          "release_date": "2020-09-04"
+        }
+      ]
     });
 
     getIndividualMovie.mockResolvedValue(
@@ -94,6 +102,30 @@ describe("App", () => {
     //quick fix: 
     //best practice take out router in app component + make sure render app in test -> wrapped in <Router history={history}
     it("Should route to a page with more movie details", async () => {
+      getMovieComments.mockResolvedValue(
+        {
+          "comments": [
+            {
+              "comment": "lalalalala",
+              "author": "iluvmoviezzz",
+              "movieId": 337401,
+              "id": 1603224854153
+            },
+            {
+              "comment": "no thankyou",
+              "author": "iluvmoviezzz",
+              "movieId": 337401,
+              "id": 1603224859706
+            },
+            {
+              "comment": "best movie ever",
+              "author": "trollsfan4ever",
+              "movieId": 337401,
+              "id": 1603224918563
+            }
+          ]
+        }
+      )
 
       render(
       <MemoryRouter>
@@ -159,7 +191,112 @@ describe("App", () => {
        await waitFor(() => expect(screen.getByText("Logout")).toBeInTheDocument())
        expect(screen.getByText("Your rating: 3")).toBeInTheDocument()
     })
+    it("should display a movie page with comment form if a user is logged in", async () => {
+      postUserLogin.mockResolvedValue(
+        {
+          "user": {
+            "email": "marge@turing.io",
+            "id": 84,
+            "name": "Marge"
+          }
+        }
+      )
 
+      getUserRatings.mockResolvedValue(
+        { "ratings": 
+          [
+            {
+              "id": 2743,
+              "user_id": 84,
+              "movie_id": 694919,
+              "rating": 3,
+              "created_at": "2020-10-15T00:41:26.504Z",
+              "updated_at": "2020-10-15T00:41:26.504Z"
+            },
+            {
+              "id": 2747,
+              "user_id": 84,
+              "movie_id": 413518,
+              "rating": 4,
+              "created_at": "2020-10-15T01:28:13.417Z",
+              "updated_at": "2020-10-15T01:28:13.417Z"
+            },
+            {
+              "id": 2749,
+              "user_id": 84,
+              "movie_id": 550231,
+              "rating": 4,
+              "created_at": "2020-10-15T01:37:34.555Z",
+              "updated_at": "2020-10-15T01:37:34.555Z"
+            }
+          ]
+        }
+      )
+      getIndividualMovie.mockResolvedValueOnce(
+        {
+          movie:
+          {
+            average_rating: 3,
+            backdrop_path:
+              "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+            budget: 200000000,
+            genres: ["Action", "Adventure", "Drama", "Fantasy"],
+            id: 337401,
+            overview:
+              "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
+            poster_path:
+              "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+            release_date: "2020-09-04",
+            revenue: 57000000,
+            runtime: 115,
+            tagline: "",
+            title: "Mulan",
+          },
+        });
+      getMovieComments.mockResolvedValue(
+        {
+          "comments": [
+            {
+              "comment": "lalalalala",
+              "author": "iluvmoviezzz",
+              "movieId": 337401,
+              "id": 1603224854153
+            },
+            {
+              "comment": "no thankyou",
+              "author": "iluvmoviezzz",
+              "movieId": 337401,
+              "id": 1603224859706
+            },
+            {
+              "comment": "best movie ever",
+              "author": "trollsfan4ever",
+              "movieId": 337401,
+              "id": 1603224918563
+            }
+          ]
+        }
+      )
+
+      render (
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      )
+      userEvent.click(screen.getByRole("link", {name: "Login"}));
+      userEvent.type(screen.getByPlaceholderText("email"), "marge@turing.io");
+      userEvent.type(screen.getByPlaceholderText("password"), "password123");
+      userEvent.click(screen.getByRole("button", {name: "Submit"}));
+      await waitFor(() => expect(screen.getByText("Logout")).toBeInTheDocument())
+       
+      const movieTitle = screen.getByAltText("poster of Mulan");
+      expect(movieTitle).toBeInTheDocument()
+
+      userEvent.click(screen.getByRole('link', {name: /mulan/i}))
+      await waitFor(() => screen.debug())
+      const mulanRuntime = screen.getByText("Runtime: 115 minutes")
+      expect(mulanRuntime).toBeInTheDocument();
+    })
   });
 })
 
